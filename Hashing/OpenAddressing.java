229af0f130f118c4ad93ba8f74175609fa196a33
akshat <akshat@gmail.com>
2022-06-30 23:49:29 +0530

    Hashing

---
+++ Hashing/OpenAddressing.java
@@ -0,0 +1,21 @@
+
+public class OpenAddressing {
+    // class MyHash {
+    //     int arr[];
+    //     int cap, size;
+
+    //     MyHash(int c) {
+    //         cap = c;
+    //         size = 0;
+    //         for (int i = 0; i < cap; i++) {
+    //             arr[i] = -1;
+    //         }
+    // static int hash(int key){
+    //     return key % cap;
+    // }
+    public static void main(String[] args) {
+        
+    }
+        }
+    }
+}
---
+++ Hashing/chainingImplementation.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import Array.ArrayListin;
+
+public class chainingImplementation {
+    int BUCKET;
+    ArrayList<LinkedList<Integer>> table;
+
+    Myhash(int b){
+        BUCKET = b;
+        table = new ArrayList<LinkedLis<Integer>>();
+        for (int i = 0; i < b; i++) {
+            table.add(new LinkedList<Integer>());
+        }
+        // static void insert(int key){
+        //     int i = key % BUCKET;
+        //     table.get(i).add(key);
+        // }
+        // static void remove(int key){
+        //     int i = key % BUCKET;
+        //     table.get(i).remove((Integer)i);
+        // }
+    }
+}
